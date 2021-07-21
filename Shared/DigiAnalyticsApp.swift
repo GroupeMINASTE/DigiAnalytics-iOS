a6d9fa756b97eca8b826d9f5621445849e5fde3b
Nathan Fallet <contact@nathanfallet.me>
2021-07-21 11:55:47 +0200

    Initial Commit

---
+++ Shared/DigiAnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiAnalyticsApp.swift
+//  Shared
+//
+//  Created by Nathan FALLET on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct DigiAnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
